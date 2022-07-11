418a182a1c2e70e47f2a519f47d87a2fb44458d9
Irham F Rufdo <irham.f.rufdo@gmail.com>
2022-07-11 10:50:59 +0700

    Day 4 - Creating Blog Page

---
+++ assets/js/myblog.js
@@ -0,0 +1,76 @@
+let myblogs = []
+
+function addBlog(event) {
+    event.preventDefault()
+
+    let title = document.getElementById("input-blog-title").value
+    let content = document.getElementById("input-blog-content").value
+    let image = document.getElementById("input-blog-image")
+
+    image = URL.createObjectURL(image.files[0])
+
+    let myblog = {
+        title,
+        content,
+        image
+    }
+
+    myblogs.push(myblog)
+    rendermyblogs()
+}
+
+function rendermyblogs() {
+    console.log(myblogs);
+
+    let containerBlogs = document.getElementById("contents")
+
+    containerBlogs.innerHTML = ""
+
+    for (let i = 0; i < myblogs.length; i++) {
+        containerBlogs.innerHTML += `
+        <div style="flex-direction: row;
+        padding: 10px;
+        width: 59%;
+        display: block;
+        margin: 3%;
+        border: none;
+        padding: 40px 0px 40px 50px;
+        border-radius: 10px;
+        background-color: white;">
+            <div style="width: 100%;
+            display: block;">
+                <img src="${myblogs[i].image}" alt="" />
+            </div>
+            <div>
+                <div>
+                    <button style="background-color: #ec4437;
+                    color: white;
+                    border: none;
+                    padding: 8px 30px;
+                    border-radius: 30px;
+                    cursor: pointer;">Edit Post</button>
+                    <button style="background-color: #ec4437;
+                    color: white;
+                    border: none;
+                    padding: 8px 30px;
+                    border-radius: 30px;
+                    cursor: pointer;">Post Blog</button>
+                </div>
+                <h1>
+                    <a href="myblog-detail.html" target="_blank">${myblogs[i].title}</a>
+                </h1>
+                <div>
+                    8 Jul 2022 4:35 WIB | Irham Fatriyandas Rufdo
+                </div>
+                <p>
+                    ${myblogs[i].content}
+                </p>
+                <div style="text-align: right;
+                margin-right: 20px">
+                    <span style="font-size: 15px; color: grey;">0 minutes ago</span>
+                </div>
+            </div>
+        </div>
+        `
+    }
+}
