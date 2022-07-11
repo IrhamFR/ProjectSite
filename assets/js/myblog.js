3c8abc63ceb3c5aaf03c7f85955ed5867fb4b703
Irham F Rufdo <irham.f.rufdo@gmail.com>
2022-07-11 12:53:36 +0700

    update Dumbways/Day 4 - Creating Blog Page

--- assets/js/myblog.js
+++ assets/js/myblog.js
@@ -1,5 +1,20 @@
 let myblogs = []
 
+let month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
 function addBlog(event) {
     event.preventDefault()
 
@@ -12,7 +27,8 @@ function addBlog(event) {
     let myblog = {
         title,
         content,
-        image
+        image,
+        postedAt: new Date()
     }
 
     myblogs.push(myblog)
@@ -20,7 +36,6 @@ function addBlog(event) {
 }
 
 function rendermyblogs() {
-    console.log(myblogs);
 
     let containerBlogs = document.getElementById("contents")
 
@@ -28,49 +43,74 @@ function rendermyblogs() {
 
     for (let i = 0; i < myblogs.length; i++) {
         containerBlogs.innerHTML += `
-        <div style="flex-direction: row;
-        padding: 10px;
-        width: 59%;
-        display: block;
-        margin: 3%;
-        border: none;
-        padding: 40px 0px 40px 50px;
-        border-radius: 10px;
-        background-color: white;">
-            <div style="width: 100%;
-            display: block;">
+        <div class="blog-list-item">
+            <div class="blog-image">
                 <img src="${myblogs[i].image}" alt="" />
             </div>
-            <div>
-                <div>
-                    <button style="background-color: black;
-                    color: white;
-                    border: none;
-                    padding: 8px 30px;
-                    border-radius: 30px;
-                    cursor: pointer;">Edit Post</button>
-                    <button style="background-color: black;
-                    color: white;
-                    border: none;
-                    padding: 8px 30px;
-                    border-radius: 30px;
-                    cursor: pointer;">Post Blog</button>
+            <div class="blog-content">
+                <div class="btn-group">
+                    <button class="btn-edit">Edit Post</button>
+                    <button class="btn-post">Post Blog</button>
                 </div>
                 <h1>
-                    <a style="text-decoration:none; color: black;" href="myblog-detail.html" target="_blank">${myblogs[i].title}</a>
+                    <a href="myblog-detail.html" target="_blank">${myblogs[i].title}</a>
                 </h1>
-                <div>
-                    11 Jul 2022 10:00 WIB | Irham Fatriyandas Rufdo
+                <div class="detail-blog-content">
+                    ${getFullTime(myblogs[i].postedAt)} | Irham Fatriyandas Rufdo
                 </div>
                 <p>
                     ${myblogs[i].content}
                 </p>
+                <br>
+                <br>
+                <br>
                 <div style="text-align: right;
                 margin: 20px">
-                    <span style="font-size: 15px; color: grey;">0 minutes ago</span>
+                    <span style="font-size: 15px; color: grey;">${getDistanceTime(myblogs[i].postedAt)}</span>
                 </div>
             </div>
-        </div>
+        </div>  
         `
     }
 }
+
+function getFullTime(time) {
+    let date = time.getDate()
+    let monthIndex = time.getMonth()
+    let year = time.getFullYear()
+
+    let hour = time.getHours()
+    let minute = time.getMinutes()
+
+    return `${date} ${month[monthIndex]} ${year} ${hour}:${minute} WIB`
+}
+
+function getDistanceTime(time) {
+    let distance = new Date() - new Date(time)
+
+    let miliseconds = 1000
+    let secondInMinutes = 60
+    let minuteInHour = 60
+    let secondInHour = secondInMinutes * minuteInHour
+    let hourInDay = 23
+
+    let dayDistance = distance / (miliseconds * secondInHour * hourInDay)
+
+    if (dayDistance >= 1) {
+        const dayDate = Math.floor(dayDistance) + ' day ago'
+        return dayDate
+    } else {
+        let hourDistance = Math.floor(distance / (miliseconds * secondInHour))
+        if (hourDistance > 0) {
+            return hourDistance + ' hour ago'
+        } else {
+            let minuteDistance = Math.floor(distance / (miliseconds * secondInMinutes))
+            return minuteDistance + ' minute ago'
+        }
+    }
+
+}
+
+setInterval(function () {
+    rendermyblogs()
+}, 2000)
